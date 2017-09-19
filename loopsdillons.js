b6417237a10a9fe85a5c384fb1ef8b894c8cbc1a
9dbartlett <31480988+9dbartlett@users.noreply.github.com>
2017-09-19 09:09:38 -0500

    Add files via upload

--- loopsdillons.js
+++ loopsdillons.js
@@ -13,15 +13,22 @@ console.log("Done Son!");
 }
 
 
-function loopTwo(){
+function loopTwo(number, power){
+  let sum = 0;
+  for(let p=0; p <=power; p++){
+    sum = sum + Math.pow(number,power)
+    number = number ++
+  }
+  return sum
+  }
 
+function loopThree(symbol, number){
+  for(let p=0; p<=number;){
+    console.log(symbol);
+    symbol=symbol++
+  }
 
-}
-
-function loopThree(){
-
-
-}
+  }
 
 function main(){
     //Testing each function
